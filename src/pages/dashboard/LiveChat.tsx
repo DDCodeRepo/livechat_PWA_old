a0fec7356f1004a0e382d2c61d40200d2a7da57f
akashmistry <akashmistryofficial@gmail.com>
2022-05-19 17:53:08 +0530

    initialcommit

---
+++ src/pages/dashboard/LiveChat.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import type { FC } from "react";
+import { Helmet } from "react-helmet-async";
+import { Box } from "@material-ui/core";
+import { ChatSidebar, ChatThread } from "../../components/dashboard/chat";
+import gtm from "../../lib/gtm";
+import { getThreads } from "../../slices/chat";
+import { useDispatch } from "../../store";
+
+const LiveChat: FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    gtm.push({ event: "page_view" });
+  }, []);
+
+  useEffect(() => {
+    dispatch(getThreads());
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>Dashboard: Live Chat | Material Kit Pro</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: "background.default",
+          display: "flex",
+          height: "100%",
+        }}
+      >
+        <ChatSidebar />
+        <ChatThread />
+        {/* <ChatSidebar /> */}
+      </Box>
+    </>
+  );
+};
+
+export default LiveChat;
